e6ff130d681291c63d19cb2e67399af6a78a4c7c
AbdAlftahSalem <abd321alftah@gmail.com>
2025-03-18 11:29:51 +0200

    add auto translation functionality and update dependencies for lingo_hunter

--- lib/src/lingo_hunter_base.dart
+++ lib/src/lingo_hunter_base.dart
@@ -1,43 +1,26 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:translator/translator.dart';
+
 abstract class LingoHunter {
   /// Extracts translatable strings from a Flutter project and generates translation files
   static Future<void> extractAndCreateTranslationFiles({
-    /// The base language where the key is the same as the value
     required String baseLang,
-
-    /// List of additional languages for which translation files will be created
     required List<String> langs,
-
-    /// The Flutter project directory (optional, will auto-detect if not provided)
     String? projectDirectory,
-
-    /// The output directory for the translation files (optional, defaults to the project root)
     String? outputDirectory,
-
-    /// Whether to translate the base language
     bool translateBaseLang = true,
-
-    /// Additional regular expressions to search for translation strings
     List<RegExp>? additionalRegExps,
-
-    /// Whether to override default regular expressions entirely
     bool overrideRegExps = false,
-
-    /// File extensions to scan for translation strings
     List<String> fileExtensions = const ['.dart'],
   }) async {
-    // Find the project root directory
     final String projectRoot = projectDirectory ?? await _findLibDirectory();
-
-    // Use the project root as output directory if not specified
     final String outputDir = outputDirectory ?? projectRoot;
 
     print("Project root directory: $projectRoot");
     print("Output directory: $outputDir");
 
-    // Extract translatable strings
     final Set<String> strings = await extractStringsFromFlutterProject(
       directory: projectRoot,
       additionalRegExps: additionalRegExps,
@@ -45,7 +28,6 @@ abstract class LingoHunter {
       fileExtensions: fileExtensions,
     );
 
-    // Generate translation files
     await _createTranslationFiles(
       strings: strings,
       outputDirectory: outputDir,
@@ -57,16 +39,14 @@ abstract class LingoHunter {
     print("Successfully extracted strings and generated translation files.");
   }
 
-  /// Finds the project's root directory by searching for the `pubspec.yaml` file
   static Future<String> _findLibDirectory() async {
     Directory current = Directory.current;
 
-    // Search for `pubspec.yaml` in the current directory and parent directories
     while (true) {
       if (await File('${current.path}/pubspec.yaml').exists()) {
         final libPath = '${current.path}/lib';
         if (await Directory(libPath).exists()) {
-          return libPath; // Return the `lib` directory if found
+          return libPath;
         } else {
           print(
               "Warning: 'lib' directory not found. Returning project root instead.");
@@ -74,7 +54,6 @@ abstract class LingoHunter {
         }
       }
 
-      // Ensure we haven't reached the system root
       if (current.path == current.parent.path) {
         break;
       }
@@ -82,40 +61,35 @@ abstract class LingoHunter {
       current = current.parent;
     }
 
-    // If `pubspec.yaml` was not found, default to the current directory
     print(
         "Warning: `pubspec.yaml` not found. Using current directory as project root.");
     return Directory.current.path;
   }
 
-  /// Extracts translatable strings from a Flutter project
   static Future<Set<String>> extractStringsFromFlutterProject({
     required String directory,
     List<RegExp>? additionalRegExps,
     bool overrideRegExps = false,
     List<String> fileExtensions = const ['.dart'],
   }) async {
-    // Default patterns to search for translation calls
     final List<RegExp> defaultPatterns = [
-      RegExp(r'"([^"]+)"\.tr\(\)'), // "string".tr()
-      RegExp(r"'([^']+)'\.tr\(\)"), // 'string'.tr()
-      RegExp(r'"([^"]+)"\.tr'), // "string".tr
-      RegExp(r"'([^']+)'\.tr"), // 'string'.tr
-      RegExp(r'"([^"]+)"\.tr\(\w+\)'), // "string".tr(context)
-      RegExp(r"'([^']+)'\.tr\(\w+\)"), // 'string'.tr(context)
-      RegExp(r'context\.tr\("([^"]+)"\)'), // context.tr("string")
-      RegExp(r"context\.tr\('([^']+)'\)"), // context.tr('string')
-      RegExp(r'tr\(\w+, "([^"]+)"\)'), // tr(context, "string")
-      RegExp(r"tr\(\w+, '([^']+)'\)"), // tr(context, 'string')
-      RegExp(r'tr\("([^"]+)"\)'), // tr("string")
-      RegExp(r"tr\('([^']+)'\)"), // tr('string')
-      RegExp(r'"([^"]+)"\.tr\(args: \[.*?\]\)'), // "string".tr(args: [])
-      RegExp(r'"([^"]+)"\.plural\(\d+\)'), // "string".plural(3)
-      //Intl Package Patterns
+      RegExp(r'"([^"]+)"\.tr\(\)'),
+      RegExp(r"'([^']+)'\.tr\(\)"),
+      RegExp(r'"([^"]+)"\.tr'),
+      RegExp(r"'([^']+)'\.tr"),
+      RegExp(r'"([^"]+)"\.tr\(\w+\)'),
+      RegExp(r"'([^']+)'\.tr\(\w+\)"),
+      RegExp(r'context\.tr\("([^"]+)"\)'),
+      RegExp(r"context\.tr\('([^']+)'\)"),
+      RegExp(r'tr\(\w+, "([^"]+)"\)'),
+      RegExp(r"tr\(\w+, '([^']+)'\)"),
+      RegExp(r'tr\("([^"]+)"\)'),
+      RegExp(r"tr\('([^']+)'\)"),
+      RegExp(r'"([^"]+)"\.tr\(args: \[.*?\]\)'),
+      RegExp(r'"([^"]+)"\.plural\(\d+\)'),
       RegExp(r'AppLocalizations\.of\(context\)!\.translate\("([^"]+)"\)'),
     ];
 
-    // Determine the patterns to use
     List<RegExp> patterns;
     if (overrideRegExps && additionalRegExps != null) {
       patterns = additionalRegExps;
@@ -130,20 +104,15 @@ abstract class LingoHunter {
     final Directory projectDirObj = Directory(directory);
     final List<FileSystemEntity> entities =
         await projectDirObj.list(recursive: true).toList();
-
-    // Filter files by the specified extensions
     final List<File> filteredFiles = entities
         .whereType<File>()
         .where((file) => fileExtensions.any((ext) => file.path.endsWith(ext)))
         .toList();
 
-    // Extract strings from files
     for (final File file in filteredFiles) {
       final String content = await file.readAsString();
-
       for (final RegExp pattern in patterns) {
         final Iterable<RegExpMatch> matches = pattern.allMatches(content);
-
         for (final RegExpMatch match in matches) {
           if (match.groupCount >= 1 && match.group(1) != null) {
             strings.add(match.group(1)!);
@@ -155,7 +124,6 @@ abstract class LingoHunter {
     return strings;
   }
 
-  /// Creates translation files for different languages
   static Future<void> _createTranslationFiles({
     required Set<String> strings,
     required String outputDirectory,
@@ -168,7 +136,8 @@ abstract class LingoHunter {
       await outputDir.create(recursive: true);
     }
 
-    // Always create the base language file, even if `translateBaseLang` is false
+    final translator = GoogleTranslator();
+
     final String baseFilePath = '$outputDirectory/translations_$baseLang.json';
     final Map<String, String> baseStrings = {
       for (final string in strings) string: translateBaseLang ? string : ""
@@ -177,15 +146,43 @@ abstract class LingoHunter {
 
     for (final String lang in langs) {
       final String langFilePath = '$outputDirectory/translations_$lang.json';
-      final Map<String, String> langStrings = {
-        for (final string in strings) string: ""
-      };
+      final Map<String, String> langStrings = {};
+
+      // Perform batch translation for the target language
+      await _batchTranslate(strings, lang, translator).then((translations) {
+        for (int i = 0; i < strings.length; i++) {
+          langStrings[strings.elementAt(i)] = translations[i];
+        }
+      }).catchError((e) {
+        print("Error during batch translation to '$lang': $e");
+      });
+
       await _writeTranslationFile(langFilePath, langStrings);
+      print("Translation file created: $langFilePath");
     }
   }
 
-  /// Helper function to write a translation file
-  /// دالة مساعدة لكتابة ملف الترجمة
+  static Future<List<String>> _batchTranslate(
+      Set<String> strings, String lang, GoogleTranslator translator) async {
+    final List<String> batch = strings.toList();
+
+    // Use Future.wait to translate multiple strings in parallel
+    final results = await Future.wait(
+      batch.map((text) async {
+        try {
+          final translatedText =
+              await translator.translate(text, from: 'en', to: lang);
+          return translatedText.text;
+        } catch (e) {
+          print("Error translating '$text' to '$lang': $e");
+          return ''; // Fallback if translation fails
+        }
+      }),
+    );
+
+    return results;
+  }
+
   static Future<void> _writeTranslationFile(
       String filePath, Map<String, String> strings) async {
     final File file = File(filePath);
